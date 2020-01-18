8796f9c07ccfec1e14e98b3f0419fd65c8ec24ab
zhouxinwei97 <45256284+zhouxinwei97@users.noreply.github.com>
2020-01-18 21:13:33 +0800

    deleted REACT

--- telebot/main.js
+++ telebot/main.js
@@ -1,39 +1,34 @@
 var nodeTelegramBotApi = require("node-telegram-bot-api")
 
-
-var token = '1059129730:AAGSsVezEKPww_9Gsw1wDcS3McgT5AnDcjo'; 
-
-var chatID = '@freelancertest'; 
-
-var html = 'Hey this your awesome message\n\n'+
-'🤖🤖🤖🤖🤖🤖🤖🤖🤖🤖\n\n'+
-'<b>bold</b>, <strong>bold</strong>\n'+
-'<i>italic</i>, <em>italic</em>\n'+
-'<a href="https://www.forex-signals.club/">inline URL</a>\n'+
-'<a href="tg://user?id=@zied_hosni">inline mention of a user</a>\n'+
-'<code>inline fixed-width code</code>\n'+
-'<pre>pre-formatted fixed-width code block</pre>\n\n'+
-'🍩🍩🍩🍩🍩🍩🍩🍩🍩🍩';
-
-
+var token = '1059129730:AAGSsVezEKPww_9Gsw1wDcS3McgT5AnDcjo';
+var chatID = '@freelancertest';
 var buttons = [
-    [
-        {"text": "See on Github", "url": "https://github.com/html5-ninja/inline-button-for-telegram-channel"}, 
-        {"text": "Follow me", "url": "https://twitter.com/zied_hosni"}
-    ],
-    [
-        {"text": "🎖 Join our forex channel 🎖", "url": "https://t.me/forex_signals_club"}
-    ]
+    [{
+        "text": "Click to Apply",
+        "url": "www.google.com"
+    }]
 ];
 
-bot = new nodeTelegramBotApi(token, {polling: true});
 
-bot.sendMessage(chatID, html, 
-    {
+
+function sendMessage(chatID, token) {
+    bot = new nodeTelegramBotApi(token, {
+        polling: true
+    });
+
+    bot.sendMessage(chatID, html, {
         parse_mode: "HTML",
-        disable_web_page_preview:true,  
+        disable_web_page_preview: true,
         "reply_markup": {
             "inline_keyboard": buttons
         }
-    }
-);
+    });
+
+
+}
+
+var html = 'test message fk u ';
+
+
+
+
